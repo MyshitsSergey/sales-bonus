c448f3529f103daec147367d855353d0d3b9b97d
Sergey Myshits <sergemysha@gmail.com>
2026-01-14 23:04:32 +0300

    mk0.1

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@
  */
 function calculateSimpleRevenue(purchase, _product) {
    // @TODO: Расчет выручки от операции
+    // purchase — это одна из записей в поле items из чека в data.purchase_records
+   // _product — это продукт из коллекции data.products
+   const { discount, sale_price, quantity } = purchase;
 }
 
 /**
@@ -17,6 +20,7 @@ function calculateSimpleRevenue(purchase, _product) {
  */
 function calculateBonusByProfit(index, total, seller) {
     // @TODO: Расчет бонуса от позиции в рейтинге
+    const { profit } = seller;
 }
 
 /**
@@ -25,7 +29,39 @@ function calculateBonusByProfit(index, total, seller) {
  * @param options
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
+
 function analyzeSalesData(data, options) {
+//1. @TODO: Проверка входных данных  
+   if(!data || !Array.isArray(data.sellers) || data.sellers.length === 0) // @TODO: Проверка входных данных // Здесь проверим входящие данные//Проверить входные данные
+        {
+            throw new Error('Некорректные входные данные');
+        }
+
+
+//2. @TODO: Проверка наличия опций
+    if(typeof options !== "object" || typeof options === NULL) // @TODO: Проверка наличия опций
+        {
+            throw new Error('Опции должны быть - Обьектом!');ы
+        }
+    const { calculateRevenue, calculateBonus } = options; // Сюда передадим функции для расчётов
+    
+    if(typeof calculateRevenue !== "function" || typeof calculateBonus !== "function")//// @TODO: Проверка явл. ли опции функциями
+        {
+            throw new Error('должны быть - Функции!');
+        }
+    
+//3. @TODO: Подготовка промежуточных данных для сбора статистики
+    const sellerStats = data.sellers.map(seller => ({
+   // Заполним начальными данными
+   }));
+
+//4. @TODO: Индексация продавцов и товаров для быстрого доступа
+   
+
+
+
+
+
     // @TODO: Проверка входных данных
 
     // @TODO: Проверка наличия опций
